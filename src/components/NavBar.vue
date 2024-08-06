78dbb55670c3b45d24ba911c366c907eab344153
Krizu9 <kristian.pekkanen6@gmail.com>
2024-08-06 19:20:18 +0300

    first commit, moving project to this repository

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Gymdiary-app</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink to="/login" class="nav-link-button">Login</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link-button">Home</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+
+</style>
